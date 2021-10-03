90515bdcd017a47056ded2a6a6ab0952f573463c
mihails.kuzmins <mihails.kuzmins@daytongroup.lv>
2021-10-03 14:12:42 +0300

    save file separately

--- cmd/mockgen/main.go
+++ cmd/mockgen/main.go
@@ -6,23 +6,31 @@ import (
 	"os"
 	"path/filepath"
 
+	"github.com/MyNihongo/codegen"
 	gen "github.com/MyNihongo/mockgen/internal/generator"
 	"github.com/MyNihongo/mockgen/internal/loader"
 )
 
+type execResult struct {
+	file    *codegen.File
+	pkgName string
+}
+
 func main() {
 	if wd, err := os.Getwd(); err != nil {
 		fmt.Println(err)
-	} else if pkgName, err := execute(wd, os.Args, 1); err != nil {
+	} else if result, err := execute(wd, os.Args, 1); err != nil {
+		fmt.Println(err)
+	} else if err = save(result.file, wd); err != nil {
 		fmt.Println(err)
 	} else {
-		fmt.Printf("mock generated: %s\n", pkgName)
+		fmt.Printf("mock generated: %s\n", result.pkgName)
 	}
 }
 
-func execute(wd string, mocks []string, offset int) (string, error) {
+func execute(wd string, mocks []string, offset int) (*execResult, error) {
 	if pkg, err := loader.LoadPackage(wd); err != nil {
-		return "", err
+		return nil, err
 	} else {
 		mockDecls := make([]*gen.MockDecl, len(mocks)-offset)
 
@@ -30,9 +38,9 @@ func execute(wd string, mocks []string, offset int) (string, error) {
 			mockName := gen.GetMockName(mocks[i])
 
 			if typeObj := pkg.Types.Scope().Lookup(mockName.TypeName()); typeObj == nil {
-				return "", fmt.Errorf("type %s is not found in %s", mockName.TypeName(), wd)
+				return nil, fmt.Errorf("type %s is not found in %s", mockName.TypeName(), wd)
 			} else if structType, ok := typeObj.Type().Underlying().(*types.Struct); !ok {
-				return "", fmt.Errorf("type %s is not a struct", mockName.TypeName())
+				return nil, fmt.Errorf("type %s is not a struct", mockName.TypeName())
 			} else {
 				fields := make([]*gen.FieldDecl, structType.NumFields())
 
@@ -53,10 +61,19 @@ func execute(wd string, mocks []string, offset int) (string, error) {
 		}
 
 		if file, err := gen.GenerateMocks(wd, pkg.Name, mockDecls); err != nil {
-			return "", err
+			return nil, err
 		} else {
-			path := filepath.Join(wd, "mock_gen_test.go")
-			return pkg.Name, file.Save(path)
+			res := &execResult{
+				file:    file,
+				pkgName: pkg.Name,
+			}
+
+			return res, nil
 		}
 	}
+}
+
+func save(file *codegen.File, wd string) error {
+	path := filepath.Join(wd, "mock_gen_test.go")
+	return file.Save(path)
 }
